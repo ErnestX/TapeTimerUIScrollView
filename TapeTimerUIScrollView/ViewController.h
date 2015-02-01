c1ae3a327af0742903fae6f94ab43aac5aeef970
ErnestX <ernestxiangjlnq2011@gmail.com>
2015-01-31 21:03:08 -0800

    Initial Commit

---
+++ TapeTimerUIScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TapeTimerUIScrollView
+//
+//  Created by Jialiang Xiang on 2015-01-31.
+//  Copyright (c) 2015 Jialiang Xiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
